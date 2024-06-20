1a20dda8c976da9d87e99d6697b8b100ac985036
mirshaduk <119918613+mirshaduk@users.noreply.github.com>
2024-06-20 13:39:21 +0530

    Update introduction.cpp
    #include<iostream>  //this is a headetr
    
    using namespace std;
    
    int main()
    {
        cout<<"Hello world";  //  Tis will print out hello world in the user screen
    
        return 0;
    }

---
+++ introduction.cpp
@@ -0,0 +1,10 @@
+#include<iostream>  //this is a headetr
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world";  //  Tis will print out hello world in the user screen
+
+    return 0;
+}
